e25563d4dc1212eade046a8738dfbc7f9a03faac
I don’t codddde <120886940+asc2563@users.noreply.github.com>
2026-08-30 17:49:11 -0700

    gg

--- src/constants/socket.ts
+++ src/constants/socket.ts
@@ -1,6 +1,8 @@
-export const DEFAULT_SOCKET_URL =
-    typeof window !== "undefined" && window.location.protocol === "https:"
-        ? "wss://localhost:5180/ws"
-        : "ws://localhost:5180/ws";
+// export const DEFAULT_SOCKET_URL =
+//     typeof window !== "undefined" && window.location.protocol === "https:"
+//         ? "wss://localhost:5180/ws"
+//         : "ws://localhost:5180/ws";
+
+export const DEFAULT_SOCKET_URL = "ws://localhost:5180/ws";
 
 export const SOCKET_URL = DEFAULT_SOCKET_URL;
